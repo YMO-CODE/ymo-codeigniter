bd78cb5a9926962be6651ae62ae51d34e67ae2be
ayush dubey <ayush@ayushdubey.in>
2026-09-02 12:49:29 +0530

    updated

--- application/views/marketing/partials/supplemental_sections.php
+++ application/views/marketing/partials/supplemental_sections.php
@@ -6,20 +6,29 @@ $body_faq_html = isset($body_faq_html) ? $body_faq_html : '';
 $has_pricing = is_array($pricing_tiers) && $pricing_tiers !== array();
 $has_faq = is_array($faq) && $faq !== array();
 $has_body_faq = trim((string) $body_faq_html) !== '';
+$has_faq_content = $has_faq || $has_body_faq;
+$side_by_side = $has_pricing && $has_faq_content;
+$grid_class = 'ymo-supplement-grid'.($side_by_side ? ' ymo-supplement-grid--split' : '');
 ?>
-<?php if ($has_pricing || $has_faq || $has_body_faq): ?>
-<div class="ymo-supplement-stack">
+<?php if ($has_pricing || $has_faq_content): ?>
+<div class="<?= html_escape($grid_class); ?>">
     <?php if ($has_pricing): ?>
-        <?= marketing_render_pricing_section_html($pricing_tiers); ?>
+        <div class="ymo-supplement-grid__col ymo-supplement-grid__col--pricing">
+            <?= marketing_render_pricing_section_html($pricing_tiers); ?>
+        </div>
     <?php endif; ?>
-    <?php if ($has_body_faq): ?>
-        <?= $body_faq_html; ?>
-    <?php endif; ?>
-    <?php if ($has_faq): ?>
-        <?php $this->load->view('marketing/partials/faq_section', array(
-            'title' => 'Popular questions',
-            'items' => $faq,
-        )); ?>
+    <?php if ($has_faq_content): ?>
+        <div class="ymo-supplement-grid__col ymo-supplement-grid__col--faq">
+            <?php if ($has_body_faq): ?>
+                <?= $body_faq_html; ?>
+            <?php endif; ?>
+            <?php if ($has_faq): ?>
+                <?php $this->load->view('marketing/partials/faq_section', array(
+                    'title' => 'Popular questions',
+                    'items' => $faq,
+                )); ?>
+            <?php endif; ?>
+        </div>
     <?php endif; ?>
 </div>
 <?php endif; ?>
